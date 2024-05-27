b220ed69c6501000b2f7d722f11f97f2a20e10d6
Riku <ree9.business@gmail.com>
2024-05-27 12:17:41 +0900

    [add] create apollo client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,24 @@
 import "./App.css";
-import { GuestRoute, PrivateRoute } from "./AuthRoute";
+import { GuestRoute, PrivateRoute } from "./libs/AuthRoute";
 import Main from "./components/Main";
 import NotFound from "./components/NotFound";
 import SiginIn from "./components/SiginIn";
 import SignUp from "./components/SignUp";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import client from "./libs/apollo.client";
+import { ApolloProvider } from "@apollo/client";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/signin" element={<GuestRoute children={<SiginIn />} />} />
-        <Route path="/signup" element={<GuestRoute children={<SignUp />} />} />
-        <Route path="/" element={<PrivateRoute children={<Main />} />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<GuestRoute children={<SiginIn />} />} />
+          <Route path="/signup" element={<GuestRoute children={<SignUp />} />} />
+          <Route path="/" element={<PrivateRoute children={<Main />} />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
