8c770be5a4751dd6c4379d240c34cbd75c52a4f8
Riku <ree9.business@gmail.com>
2024-05-29 12:48:56 +0900

    fix typo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { GuestRoute, PrivateRoute } from "./libs/AuthRoute";
 import Main from "./components/Main";
 import NotFound from "./components/NotFound";
-import SiginIn from "./components/SiginIn";
+import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import client from "./libs/apollo.client";
@@ -12,7 +12,7 @@ function App() {
     <ApolloProvider client={client}>
       <BrowserRouter>
         <Routes>
-          <Route path="/signin" element={<GuestRoute children={<SiginIn />} />} />
+          <Route path="/signin" element={<GuestRoute children={<SignIn />} />} />
           <Route path="/signup" element={<GuestRoute children={<SignUp />} />} />
           <Route path="/" element={<PrivateRoute children={<Main />} />} />
           <Route path="*" element={<NotFound />} />
